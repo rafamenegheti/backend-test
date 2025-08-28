c063ce1181e1f231307e44ea478dfe502a17eea5
Rafael Menegheti <rafaelmenegheti51@gmail.com>
2025-08-28 19:50:55 -0300

    chore: adding env verification on client

--- src/database/client.ts
+++ src/database/client.ts
@@ -1,5 +1,11 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 
-export const db = drizzle(process.env.DATABASE_URL, {
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL environment variable is not set");
+}
+
+export const db = drizzle(databaseUrl, {
   logger: process.env.NODE_ENV === "development",
 });
